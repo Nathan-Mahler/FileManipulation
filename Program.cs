c774483e8f561ba9112a26ee2b3aaeae15c3e180
Nathan Mahler <Nathan.Mahler@selecthealth.org>
2025-04-18 15:11:37 -0600

    file system watcher class

--- Program.cs
+++ Program.cs
@@ -8,55 +8,62 @@ namespace PSJR_FilePractice
         static void Main(string[] args)
         {
             WriteLine("Parsing command line options");
-            var command = args[0];
+            var directoryToWatch = args[0];
+            if (!Directory.Exists(directoryToWatch))
+            {
+                WriteLine($"{directoryToWatch} does not exist.");
+                WriteLine("Press enter to quit");
+                ReadLine();
+                return;
+            }
+            WriteLine($"Watching directory: {directoryToWatch}");
+            using FileSystemWatcher inputFileWatcher = new FileSystemWatcher(directoryToWatch);
 
-            if (command == "--file")
-            {
-                string filePath = args[1];
-                //check if path is absolute or relative
-                if (!Path.IsPathFullyQualified(filePath))
-                {
-                    WriteLine($"File path is not absolute: {filePath}");
-                    ReadLine();
-                    return;
-                }
-                WriteLine($"Processing file: {filePath}");
-                ProcessSingleFile(filePath);
-            }
-            else if (command == "--dir")
-            {
-                string directoryPath = args[1];
-                string fileType = args[2];
-                WriteLine($"Processing directory: {directoryPath} + for {fileType} files");
-                ProcessDirectory(directoryPath, fileType);
-            }
-            WriteLine("Done");
+            inputFileWatcher.IncludeSubdirectories = false;
+            inputFileWatcher.InternalBufferSize = 32_768; // 32kB is the size of this property
+            inputFileWatcher.Filter = "*.*"; //this is the default
+            inputFileWatcher.NotifyFilter = NotifyFilters.FileName;
+
+            inputFileWatcher.Created += FileCreated;
+            inputFileWatcher.Changed += FileChanged;
+            inputFileWatcher.Deleted += FileDeleted;
+            inputFileWatcher.Renamed += FileRenamed;
+            inputFileWatcher.Error += WatcherError;
+
+            inputFileWatcher.EnableRaisingEvents = true;
+            WriteLine("Press enter to quit");
             ReadLine();
         }
+
+        private static void WatcherError(object sender, ErrorEventArgs e)
+        {
+            WriteLine($"Folder PS data in no longer being watched  with error: {e.GetException}");
+        }
+
+        private static void FileRenamed(object sender, RenamedEventArgs e)
+        {
+            WriteLine($"File renamed: {e.FullPath} - e type: {e.ChangeType}");
+        }
+
+        private static void FileDeleted(object sender, FileSystemEventArgs e)
+        {
+            WriteLine($"File deleted: {e.FullPath} - e type: {e.ChangeType}");
+        }
+
+        private static void FileChanged(object sender, FileSystemEventArgs e)
+        {
+            WriteLine($"File changed: {e.FullPath} - e type: {e.ChangeType}");
+        }
+
+        private static void FileCreated(object sender, FileSystemEventArgs e)
+        {
+            WriteLine($"File created: {e.FullPath} - e type: {e.ChangeType}");
+        }
+
         private static void ProcessSingleFile(string filePath)
         {
             FileProcessor fileProcessor = new FileProcessor(filePath);
             fileProcessor.Process();
         }
-        private static void ProcessDirectory(string directoryPath, string fileType)
-        {
-            switch (fileType) 
-            {
-                case "TEXT":
-                    string[] txtFiles = Directory.GetFiles(directoryPath, "*.txt");//overload of GetFiles method where we are searching a directory for a certain type of file
-                    foreach (string txtFile in txtFiles) 
-                    { 
-                        var fileProcessor = new FileProcessor(txtFile);
-                        fileProcessor.Process();
-                    }
-                    break;
-                default:
-                    WriteLine($"File type {fileType} not supported");
-                    break;
-            }
-
-        }
-
-       
     }
 }
